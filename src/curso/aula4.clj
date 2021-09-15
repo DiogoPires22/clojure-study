882c3670711fcccc22c9da57444a3d8698f473bc
DiogoPires22 <diogo.pcaires@gmail.com>
2021-09-15 18:28:36 -0300

    new classes

---
+++ src/curso/aula4.clj
@@ -0,0 +1,29 @@
+(ns curso.aula4)
+; HIGHER ORDER FUNCTIONS
+; IMUTABILITY
+
+(defn valor-maior-que-100?
+  [valor-bruto]
+  (> valor-bruto 100))
+
+(defn valor-descontado
+  "Retorna o com desconto de 10% se o valor bruto for maior que 100"
+  [aplica? valor-bruto]
+  (if (aplica? valor-bruto)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println "função como parametro")
+(println (valor-descontado valor-maior-que-100? 100))
+(println (valor-descontado valor-maior-que-100? 10000))
+
+
+(def mais-caro-que-100? (fn [valor] (> valor 100)))
+(println (valor-descontado mais-caro-que-100? 10000))
+
+; NAMELESS FUNCTION
+(println (valor-descontado (fn [v] (> v 100)) 10000))
+(println (valor-descontado #(> %1 100) 10000))
+(println (valor-descontado #(> % 100) 10000))
---
+++ src/curso/aula5.clj
@@ -0,0 +1,73 @@
+(ns curso.aula5)
+
+(def precos [30 700 1000])
+
+(println (precos 1))
+
+(println (get precos 0))
+
+(println (get precos 1))
+
+; if not exists index return null
+(println (get precos 17))
+; default value if null
+(println (get precos 17 0))
+
+; persist a new value and return a new vector
+(def newPrecos (conj precos 5))
+
+(println newPrecos)
+
+
+; update index 1 increasing 1
+(println (update precos 0 inc))
+
+; update index 1 decreasing 1
+(println (update precos 1 dec))
+
+(defn soma-1
+  [valor]
+  (println  "estou somando 1 em " valor)
+  (+ valor 1))
+
+(println (update precos 1 soma-1))
+
+; código de aulas passadas
+
+(defn aplica-desconto?
+  [valor]
+  (> valor 100))
+
+(defn valor-descontado
+  [valor-bruto]
+  (if (aplica-desconto? valor-bruto)
+    ( let [taxa-de-juros (/ 10 100)
+           desconto (* valor-bruto taxa-de-juros)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println (valor-descontado 100))
+
+; MAP
+
+(println (map valor-descontado precos))
+
+; FILTER
+(println (filter even? (range 10)))
+
+(println
+  (map valor-descontado
+    (filter aplica-desconto? precos)))
+
+; REDUCE
+
+(println (reduce + precos))
+
+
+(defn minha-soma
+  [v1 v2]
+  (+ v1  v2))
+
+(println (reduce minha-soma precos))
+(println (reduce minha-soma (range 10)))
+(println (reduce minha-soma 0 [15]))
